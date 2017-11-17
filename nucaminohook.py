221757c9b3901613bb73d6194e3d6c394d443550
jzpero <jho363@uwo.ca>
2017-11-17 12:26:39 -0500

    Created NucAminoAligner class and cleaned up code

--- nucaminohook.py
+++ nucaminohook.py
@@ -2,31 +2,40 @@ import subprocess
 import os
 import csv
 import re
-cwd = os.getcwd()+'/'
-nucamino_dir = '/'.join(cwd.split('/')[:-2]+['NucAmino','build'])+'/'
-print nucamino_dir
 
-def nucamino_align(file): 
-    filename = cwd+str(file.split('.fasta')[0])
-    print filename
-    args = (nucamino_dir+"nucamino hiv1b -i {}.fasta -g=POL -o {}.tsv".format(filename,filename)).split()
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-    popen.wait()
+class NucAminoAligner():
+    def __init__(self, filename):
+        self.inputname = filename
+        self.outputname = filename.replace('fasta','tsv')
+        self.cwd = os.getcwd()+'/'
+        self.nucamino_dir = '/'.join(self.cwd.split('/')[:-2]+['NucAmino','build'])+'/'
 
-def parse_nucamino_results(filename):
-    filename = cwd + filename
-    outdict = {}
-    with open(filename,'r') as tsvin:
-        tsvin = csv.reader(tsvin,delimiter='\t')
-        data = list(tsvin)
-        shift = int(data[1][1]) - 1
-        mutationpairs = data[1][5].split(',')
-        #print mutationpairs
-        pos = [int(re.findall(r'\d+',x.split(':')[0])[0])-shift for x in mutationpairs]
-        res = [re.findall(r'\D+',x.split(':')[0][1:])[0] for x in mutationpairs]
-        muts = dict(zip(pos, res))
-    return muts
+    def align_file(self): 
+        args = (self.nucamino_dir+"nucamino hiv1b -i {} -g=POL -o {}".format(self.inputname,self.outputname)).split()
+        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+        popen.wait()
+
+    def get_mutations(self):
+        '''
+        From the tsv output of NucAmino, parses and adjusts indices and returns as two lists.
+        :return: list of sequence names, list of sequence mutation dictionaries.
+        '''
+        outdict = {}
+        muts = []
+        names = []
+        with open(self.cwd+self.outputname,'r') as tsvin:
+            tsvin = csv.reader(tsvin, delimiter='\t')
+            next(tsvin)
+            for row in tsvin:
+                names.append(row[0])
+                shift = int(row[1]) - 1
+                mutationpairs = row[5].split(',')
+                pos = [int(re.findall(r'\d+',x.split(':')[0])[0])-shift for x in mutationpairs]
+                res = [re.findall(r'\D+',x.split(':')[0][1:])[0] for x in mutationpairs]
+                muts.append(dict(zip(pos, res)))
+        return names, muts
 
 if __name__ == '__main__':
-    nucamino_align('AY030621.fasta')
-    print parse_nucamino_results('AY030621.tsv')
+    n = NucAminoAligner('testsequences.fasta')
+    n.align_file()
+    print n.get_mutations()
--- sierralocal.py
+++ sierralocal.py
@@ -1,95 +1,21 @@
-from gotoh2.aligner import Aligner
-import unittest
-import seqUtils
 import score_alg
 from hivdb import HIVdb
 import os
 import argparse
 import sys
-import hxb2
-import nucaminohook
+from nucaminohook import NucAminoAligner
 
-g2 = Aligner()
 cwd = os.getcwd()
 
-
-def align(reference, query):
-    """
-    'Procrustean' alignment of query nucleotide sequence to HXB2 reference sequence
-
-    :param query: Nucleotide sequence as a string
-    :param reference: Reference sequence as a string
-    :return: Aligned sequence fitted wrt relative insertions to reference
-    """
-
-    # Use gotoh2 to align the query to reference sequence
-    query = query.strip()
-    assert type(query) is str, 'Query must be a string.'
-
-    #Gets the best alignment based on the alignments to three HXB2 genes
-    maxscore = 0
-    for gene in [hxb2.integrase,hxb2.pro,hxb2.rt,hxb2.pol]:
-        temp = g2.align(query, gene)
-        if temp[2] > maxscore:
-            aligned_query, aligned_reference, aligned_score = temp
-            maxscore = temp[2]
-
-    fitted_query = ''
-    ''' 
-    TODO:   flag indels not in sets of three
-                exceptions for indels in proximity summing to 3n
-    '''
-    # Remove insertions in the sequence relative to reference sequence
-    for idx, char in enumerate(aligned_reference):
-        # Iff the query does not have insertion relative to reference, add
-        # the nucleotide/relative deletion to the output string
-        if char != '-':
-            # let's see if replacing all '?' given by gotoh2 with the reference AA at that residue helps our scores:
-            if aligned_query[idx] == '?':
-                fitted_query += aligned_reference[idx]
-            else:
-                fitted_query += aligned_query[idx]
-
-    return fitted_query
-
-
-def translate(seq):
-    """
-    Translate nucleotide sequence into amino acid sequence.
-    Synonymous nucleotide mixtures are resolved to the corresponding residue.
-    Nonsynonymous nucleotide mixtures are encoded with '?' 
-
-    :param seq: nucleotide sequence to translate
-    :param offset: offset by X shifts sequence to the right by X bases
-    :param resolve: Boolean to resolve ambiguous AA for purposes of aligning AA
-    :param return_list: Boolean to return as a list
-
-    :return: string (default) or list of the translated amino acid sequence
-    """
-    aa_seq = seqUtils.translate_nuc(seq, offset=0, resolve=False, return_list=True)
-    return aa_seq
-
-def import_fasta(filename):
-    '''
-    Returns a dictionary from FASTA file.
-
-    :param filename: string of the filename
-    :return: dictionary of header:sequence pairs
-    '''
-    with open(filename, 'r' ) as file:
-        fasta = seqUtils.parse_fasta(file)
-    return fasta
+def parse_args():
+    parser = argparse.ArgumentParser(description='Use the HIVdb algorithm for mutation-based resistance scoring of sequences.')
+    parser.add_argument('-i', '--i', nargs='+', dest='inputfiles', type=str, help='List of input files.')
+    args = parser.parse_args(['-i','testsequences.fasta'])
+    return args
 
 def main():
-    # CLI arg parsing
-    parser = argparse.ArgumentParser(
-        description='Use the HIVdb algorithm for mutation-based resistance scoring of sequences.'
-    )
-    parser.add_argument('-i', '--i', nargs='+', dest='inputfiles', type=str, help='List of input files.')
-
-    args = parser.parse_args(['-i','KU127836.fasta'])
-    #Some of Tammy's code
-    path = os.getcwd() + '/HIVDB.xml'
+    args = parse_args()
+    path = cwd + '/HIVDB.xml'
     algorithm = HIVdb(path)
     definitions = algorithm.parse_definitions(algorithm.root)
     #print(definitions['gene'])
@@ -102,22 +28,15 @@ def main():
     comments = algorithm.parse_comments(algorithm.root)
     #print(comments)
 
-    # Parsing and processing the nucleotide sequence file
     # Scoring each sequence
     for file in args.inputfiles:
-        fastadict = import_fasta(file)
-        for header,fasta in fastadict.items():
-            nuc_seq = fasta
-            '''
-            aligned_nuc_seq = align('', nuc_seq)
-            print aligned_nuc_seq
-            print nuc_seq
-            aa_seq = translate(aligned_nuc_seq)
-            print header,aa_seq
-            '''
-            nucaminohook.nucamino_align(file)
-            seq_mutations = nucaminohook.parse_nucamino_results(cwd+'/{}.tsv'.format(file.split('.')[0]))
-            scores = score_alg.score_drugs(database, seq_mutations)
+
+        aligner = NucAminoAligner(file)
+        aligner.align_file()
+        names, res = aligner.get_mutations()
+
+        for idx, mutationlist in enumerate(res):
+            scores = score_alg.score_drugs(database, mutationlist)
             print(scores)
 
             #TODO: output to a formatted JSON consistent with the sierra-client JSON output
