71c7c9a5fba1fd37600ca5e9f6ff3f8faaf055c9
jzpero <jasperchho@gmail.com>
2018-06-04 13:56:53 -0400

    Fixed isApobecDRM() function

--- jsonwriter.py
+++ jsonwriter.py
@@ -1,201 +1,208 @@
 import json
 import xml.etree.ElementTree as xml
-import os
 import re
 import hashlib
 from hivdb import HIVdb
 import csv
 from pathlib import Path
 
+class JSONWriter():
+    def __init__(self):
+        self.names = {'3TC':'LMV'}
 
-names = {}
-names['3TC'] = 'LMV'
+        self.HIVDB_XML_PATH = str(Path('.') / 'data' / 'HIVDB.xml')
+        print("Found HIVdb file {}".format(self.HIVDB_XML_PATH))
 
-HIVDB_XML_PATH = str(Path('.') / 'data' / 'HIVDB.xml')
-print("Found HIVdb file {}".format(HIVDB_XML_PATH))
+        # Set up algorithm data
+        self.algorithm = HIVdb(self.HIVDB_XML_PATH)
+        self.root = xml.parse(self.HIVDB_XML_PATH).getroot()
+        self.version = self.root.find('ALGVERSION').text
+        self.version_date = self.root.find('ALGDATE').text
+        self.definitions = self.algorithm.parse_definitions(self.algorithm.root)
+        self.levels = self.definitions['level']
+        self.globalrange = self.definitions['globalrange']
+        self.database = self.algorithm.parse_drugs(self.algorithm.root)
+        self.comments = self.algorithm.parse_comments(self.algorithm.root)
 
-# Set up algorithm data
-algorithm = HIVdb(HIVDB_XML_PATH)
-root = xml.parse(HIVDB_XML_PATH).getroot()
-version = root.find('ALGVERSION').text
-version_date = root.find('ALGDATE').text
-definitions = algorithm.parse_definitions(algorithm.root)
-levels = definitions['level']
-globalrange = definitions['globalrange']
-database = algorithm.parse_drugs(algorithm.root)
-comments = algorithm.parse_comments(algorithm.root)
-
-with open('./data/comments.txt','w') as comment_out:
-    comment_out.write(str(comments) +'\n')
-    comment_out.write(str(definitions['comment']))
-
-# Load comments files stored locally
-with open('./data/apobec.tsv','r') as csvfile:
-    ApobecDRMs = list(csv.reader(csvfile, delimiter='\t'))
-with open('./data/INSTI-comments.csv','r') as INSTI_file:
-    INSTI_comments = dict(csv.reader(INSTI_file,delimiter='\t'))
-with open('./data/PI-comments.csv','r') as PI_file:
-    PI_comments = dict(csv.reader(PI_file,delimiter='\t'))
-with open('./data/RT-comments.csv','r') as RT_file:
-    RT_comments = dict(csv.reader(RT_file,delimiter='\t'))
+        # Load comments files stored locally
+        with open('./data/apobec.tsv','r') as csvfile:
+            self.ApobecDRMs = list(csv.reader(csvfile, delimiter='\t'))
+        with open('./data/INSTI-comments.csv','r') as INSTI_file:
+            self.INSTI_comments = dict(csv.reader(INSTI_file,delimiter='\t'))
+        with open('./data/PI-comments.csv','r') as PI_file:
+            self.PI_comments = dict(csv.reader(PI_file,delimiter='\t'))
+        with open('./data/RT-comments.csv','r') as RT_file:
+            self.RT_comments = dict(csv.reader(RT_file,delimiter='\t'))
 
 
-def validationresults(validated):
-    validationResults = {}
-    validationResults['message'] = validated
-    validationResults['level'] = None
-    return [validationResults]
+    def formatValidationResults(self, validated):
+        validationResults = []
+        for v in validated:
+            validationResults.append({'level' : v[0], 'message' : v[1]})
+        return validationResults
 
-def drugresistance(scores,genes):
-    drugResistance = {}
-    drugResistance['version'] = {}
-    drugResistance['version']['text'] = version
-    drugResistance['version']['publishDate'] = version_date
-    drugScores = []
-    # TODO: find a way to restrict drug classes, e.g. only NRTI/NNRTI for RT gene sequence...
-    for gene in genes:
-        for drugclass in definitions['gene'][gene]:
-            classlist = definitions['drugclass'][drugclass]
-            for drug in classlist:
-                # Only record data if the score is non-zero
-                #if float(scores[drug][0]) == 0.0:
-                #    continue
-                drugScore = {}
+    def formatDrugResistance(self, scores,genes):
+        drugResistance = {}
+        drugResistance['version'] = {}
+        drugResistance['version']['text'] = self.version
+        drugResistance['version']['publishDate'] = self.version_date
+        drugScores = []
+        # TODO: find a way to restrict drug classes, e.g. only NRTI/NNRTI for RT gene sequence...
+        for gene in genes:
+            for drugclass in self.definitions['gene'][gene]:
+                classlist = self.definitions['drugclass'][drugclass]
+                for drug in classlist:
+                    # Only record data if the score is non-zero
+                    #if float(scores[drug][0]) == 0.0:
+                    #    continue
+                    drugScore = {}
 
-                #Infer resistance level text from the score and globalrange
-                resistancelevel = -1
-                for key in globalrange:
-                    maximum = float(globalrange[key]['max'])
-                    minimum = float(globalrange[key]['min'])
-                    if minimum <= scores[drug][0] <= maximum:
-                        resistancelevel = str(key)
-                        break
-                resistance_text = levels[resistancelevel]
+                    #Infer resistance level text from the score and globalrange
+                    resistancelevel = -1
+                    for key in self.globalrange:
+                        maximum = float(self.globalrange[key]['max'])
+                        minimum = float(self.globalrange[key]['min'])
+                        if minimum <= scores[drug][0] <= maximum:
+                            resistancelevel = str(key)
+                            break
+                    resistance_text = self.levels[resistancelevel]
 
-                drugScore['text'] = list(resistance_text)[0] #resistance level
-                drugScore['drugClass'] = {'name':drugclass} #e.g. NRTI
-                drugScore['score'] = float(scores[drug][0]) # score for this paritcular drug
-                drugScore['drug'] = {}
-                drugScore['drug']['displayAbbr'] = drug
-                if drug in names:
-                    drugScore['drug']['name'] = names[drug]
-                else:
-                    drugScore['drug']['name'] = drug.replace('/r','')
-                drugScore['partialScores'] = []
-                # create partial score, for each mutation, datastructure
-                for index,pscore in enumerate(scores[drug][1]):
-                    pscore = float(pscore)
-                    if not pscore == 0.0:
-                        pscoredict = {}
-                        pscoredict['score'] = pscore
-                        pscoredict['mutations'] = []
-                        for combination in scores[drug][2][index]:
-                            # find the mutation classification "type" based on the gene
-                            type_ = drugclass
-                            pos = re.findall(u'([0-9]+)',combination)[0]
-                            muts = re.findall(u'(?<=[0-9])([A-Za-z])+',combination)[0]
-                            #print(pos, muts)
-                            if gene == 'IN':
-                                for key in INSTI_comments:
-                                    if pos in key and muts in key:
-                                        type_ = INSTI_comments[key]
-                                        break
-                            elif gene == 'PR':
-                                for key in PI_comments:
-                                    if pos in key and muts in key:
-                                        type_ = PI_comments[key]
-                                        break
-                            elif gene == 'RT':
-                                for key in RT_comments:
-                                    if pos in key and muts in key:
-                                        type_ = RT_comments[key]
-                                        break
-                            # do the other stuff
-                            mut = {}
-                            #combination = str(pos)+muts
-                            mut['text'] = combination.replace('d', 'Deletion')
-                            mut['comments'] = [{
-                                'text' : findComment(gene, combination, comments, definitions['comment']),
-                                'type' : type_
-                            }]
-                            mut['primaryType'] = type_
-                            pscoredict['mutations'].append(mut)                    
-                        drugScore['partialScores'].append(pscoredict)
-                drugScores.append(drugScore)
-    drugResistance['drugScores'] = drugScores
-    drugResistance['gene'] = {'name':genes[0]}
-    return [drugResistance]
+                    drugScore['text'] = list(resistance_text)[0] #resistance level
+                    drugScore['drugClass'] = {'name':drugclass} #e.g. NRTI
+                    drugScore['score'] = float(scores[drug][0]) # score for this paritcular drug
+                    drugScore['drug'] = {}
+                    drugScore['drug']['displayAbbr'] = drug
+                    if drug in self.names:
+                        drugScore['drug']['name'] = self.names[drug]
+                    else:
+                        drugScore['drug']['name'] = drug.replace('/r','')
+                    drugScore['partialScores'] = []
+                    # create partial score, for each mutation, datastructure
+                    for index,pscore in enumerate(scores[drug][1]):
+                        pscore = float(pscore)
+                        if not pscore == 0.0:
+                            pscoredict = {}
+                            pscoredict['score'] = pscore
+                            pscoredict['mutations'] = []
+                            for combination in scores[drug][2][index]:
+                                # find the mutation classification "type" based on the gene
+                                type_ = drugclass
+                                pos = re.findall(u'([0-9]+)',combination)[0]
+                                muts = re.findall(u'(?<=[0-9])([A-Za-z])+',combination)[0]
+                                #print(pos, muts)
+                                if gene == 'IN':
+                                    for key in self.INSTI_comments:
+                                        if pos in key and muts in key:
+                                            type_ = self.INSTI_comments[key]
+                                            break
+                                elif gene == 'PR':
+                                    for key in self.PI_comments:
+                                        if pos in key and muts in key:
+                                            type_ = self.PI_comments[key]
+                                            break
+                                elif gene == 'RT':
+                                    for key in self.RT_comments:
+                                        if pos in key and muts in key:
+                                            type_ = self.RT_comments[key]
+                                            break
+                                mut = {}
+                                mut['text'] = combination.replace('d', 'Deletion')
+                                mut['comments'] = [{
+                                    'text' : self.findComment(gene, combination, self.comments, self.definitions['comment']),
+                                    'type' : type_
+                                }]
+                                mut['primaryType'] = type_
+                                pscoredict['mutations'].append(mut)                    
+                            drugScore['partialScores'].append(pscoredict)
+                    drugScores.append(drugScore)
+        drugResistance['drugScores'] = drugScores
+        drugResistance['gene'] = {'name':genes[0]}
+        return [drugResistance]
 
-def alignedgenesequences(ordered_mutation_list, genes, codon_type):
-    dic = {}
-    dic['lastAA'] = None
-    dic['firstAA'] = None
-    dic['mutations'] = []
-    for a in ordered_mutation_list:
-        mutdict = {}
-        mutdict['consensus'] = a[2]
-        mutdict['position'] = int(a[0])
-        mutdict['AAs'] = a[1]
-        mutdict['isInsertion'] = a[1] == '_'
-        mutdict['isDeletion'] = a[1] == '-'
-        mutdict['isApobecDRM'] = isApobecDRM(genes[0], a[2], a[0], a[1])
-        dic['mutations'].append(mutdict)
-    return [dic]
+    def formatAlignedGeneSequences(self, ordered_mutation_list, genes, firstlastAA):
+        dic = {}
+        dic['firstAA'] = firstlastAA[0]
+        dic['lastAA'] = firstlastAA[1]
+        dic['mutations'] = []
+        for mutation in ordered_mutation_list:
+            mutdict = {}
+            mutdict['consensus'] = mutation[2]
+            mutdict['position'] = int(mutation[0])
+            mutdict['AAs'] = mutation[1]
+            mutdict['isInsertion'] = mutation[1] == '_'
+            mutdict['isDeletion'] = mutation[1] == '-'
+            mutdict['isApobecDRM'] = self.isApobecDRM(genes[0], mutation[2], mutation[0], mutation[1])
+            dic['mutations'].append(mutdict)
+        return [dic]
 
-def inputsequence(name):
-    out = {
-        'header' : name
-    }
-    return out
+    def formatInputSequence(self, name):
+        out = {
+            'header' : name
+        }
+        return out
 
-def write_to_json(filename, names, scores, genes, ordered_mutation_list, sequence_lengths, codon_types):
-    '''
-    The main function to write passed result to a JSON file
-    :param filename: the filename to write the JSON to
-    :param names: list of sequence headers
-    :param scores: list of sequence scores
-    :param genes: list of genes in pol
-    :param ordered_mutation_list: ordered list of mutations in the query sequence relative to reference
-    '''
-    out = []
-    for index, score in enumerate(scores):
-        data = {}
-        data['inputSequence'] = inputsequence(names[index])
-        data['subtypeText'] = 'NULL'
-        validationresult = ''
-        if ('RT' in genes[index] and sequence_lengths[index] < 200) or ('PR' in genes[index] and sequence_lengths[index] < 80) or ('IN' in genes[index] and sequence_lengths[index] < 200):
-            validationresult = 'WARNING'
-        if ('RT' in genes[index] and sequence_lengths[index] < 150) or ('PR' in genes[index] and sequence_lengths[index] < 60) or ('IN' in genes[index] and sequence_lengths[index] < 100):
-            validationresult = 'SEVERE WARNING'
-        if len(genes[index]) == 0:
-            validationresult = 'CRITICAL'
-        data['validationResults'] = validationresults(validationresult)
-        if not validationresult == 'CRITICAL':
-            data['alignedGeneSequences'] = alignedgenesequences(ordered_mutation_list[index], genes[index], codon_types[index])
-            data['drugResistance'] = drugresistance(score, genes[index])
-        else:
-            data['alignedGeneSequences'] = []
-            data['drugResistance'] = []
-        out.append(data)
+    def write_to_json(self, filename, names, scores, genes, ordered_mutation_list, sequence_lengths, file_firstlastAA):
+        '''
+        The main function to write passed result to a JSON file
+        :param filename: the filename to write the JSON to
+        :param names: list of sequence headers
+        :param scores: list of sequence scores
+        :param genes: list of genes in pol
+        :param ordered_mutation_list: ordered list of mutations in the query sequence relative to reference
+        '''
+        out = []
+        for index, score in enumerate(scores):
+            data = {}
+            data['inputSequence'] = self.formatInputSequence(names[index])
+            data['subtypeText'] = None
+            validation = self.validateSequence(genes[index], sequence_lengths[index])
+            data['validationResults'] = self.formatValidationResults(validation)
+            if not 'CRITICAL' in validation:
+                data['alignedGeneSequences'] = self.formatAlignedGeneSequences(ordered_mutation_list[index], genes[index], file_firstlastAA[index])
+                data['drugResistance'] = self.formatDrugResistance(score, genes[index])
+            else:
+                data['alignedGeneSequences'] = []
+                data['drugResistance'] = []
+            out.append(data)
 
-    with open('./'+filename,'w+') as outfile:
-        json.dump(out, outfile, indent=2)
-        print("Writing JSON to file {}".format(filename))
+        with open(filename,'w+') as outfile:
+            json.dump(out, outfile, indent=2)
+            print("Writing JSON to file {}".format(filename))
 
-def findComment(gene, mutation, comments, details):
-    trunc_mut = re.findall(r'\d+\D',mutation)[0] #163K
-    #print(mutation, trunc_mut)
-    pos = re.findall(u'([0-9]+)',trunc_mut)[0]
-    muts = re.findall(u'(?<=[0-9])([A-Za-z])+',trunc_mut)[0]
-    for g, mutationdict in comments.items():
-        for item in mutationdict.keys():
-            if pos in item and muts in item:
-                full_mut = mutationdict[item]
-                if full_mut in details and g == gene:
-                    return details[full_mut]['1']
+    def validateSequence(self, genes, length):
+        validation_results = []
 
-def isApobecDRM(gene, consensus, position, AA):
-    for row in ApobecDRMs[1:]:
-        if [gene, consensus, position, AA] == row:
-            return True
-    return False
+        # Length validation
+        if ('RT' in genes and length < 200) or ('PR' in genes and length < 80) or ('IN' in genes and length < 200):
+            validation_results.append(('WARNING', "The {} sequence contains just {} codons, which is not sufficient for a comprehensive interpretation.".format(genes[0], int(length))))
+        elif ('RT' in genes and length < 150) or ('PR' in genes and length < 60) or ('IN' in genes and length < 100):
+            validation_results.append('SEVERE WARNING', "The {} sequence contains just {} codons, which is not sufficient for a comprehensive interpretation.".format(genes[0], int(length)))
+        # Gene validation
+        if len(genes) == 0:
+            validation_results.append(('CRITICAL', 'oops?'))
+
+        return validation_results
+
+    def findComment(self, gene, mutation, comments, details):
+        trunc_mut = re.findall(r'\d+\D',mutation)[0] #163K
+        pos = re.findall(u'([0-9]+)',trunc_mut)[0]
+        muts = re.findall(u'(?<=[0-9])([A-Za-z])+',trunc_mut)[0]
+        for g, mutationdict in comments.items():
+            for item in mutationdict.keys():
+                if pos in item and muts in item:
+                    full_mut = mutationdict[item]
+                    if full_mut in details and g == gene:
+                        return details[full_mut]['1']
+
+    def isApobecDRM(self, gene, consensus, position, AA):
+        ls = [row[0:3] for row in self.ApobecDRMs[1:]]
+        if [gene, consensus, str(position)] in ls:
+            i = ls.index([gene, consensus, str(position)])
+            for aa in AA:
+                if aa in self.ApobecDRMs[1:][i][3]:
+                    return True
+        return False
+
+if __name__ == "__main__":
+    writer = JSONWriter()
+    print(writer.isApobecDRM("IN", "G", 163, "TRAG"))
