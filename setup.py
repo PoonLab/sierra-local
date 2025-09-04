b96522c70ae8fc9599cee8ef2a04f9e9293d80a3
William Wang <101022180+WilliamZekaiWang@users.noreply.github.com>
2025-09-04 09:27:48 -0400

    Update setup.py version number

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     'orjson>=3.9.1',
     'types-setuptools>=67.8.0.0'
     ],
-    version='0.4.1',
+    version='0.4.2',
     author=['Jasper Ho', 'Garway Ng', 'Gopi Gugan', 'Mathias Renaud', 'William Wang', 'Art Poon'],
     author_email=['jasperchho@gmail.com', 'apoon42@uwo.ca'],
     url='https://github.com/PoonLab/sierra-local',
